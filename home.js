7611597ba129c01328dd41e8206502954d612bf2
Pedp4 WPBX4125BLF092403369 <wpbx4125blf092403369@pedp4.onmicrosoft.com>
2025-08-27 11:15:29 +0600

    cash out feature added

--- home.js
+++ home.js
@@ -32,23 +32,68 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     document.getElementById('available-balance').innerText = newAddedBalance;
 })
 
+
+//cash out feature
+
+document.getElementById('btn-withdraw-money').addEventListener('click',function(event){
+    event.preventDefault();
+
+    const cashOutAmount = parseInt(document.getElementById('withdraw-amount').value);
+
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+
+//    validation
+
+    const agentNumber = document.getElementById('agent-number').value;
+
+    const pinNumber = parseInt(document.getElementById('account-pinnumber').value);
+
+    if(agentNumber.length < 11){
+        alert('Enter valid Agent Number');
+        return;
+    }
+
+    if(pinNumber !== validPinNumber){
+        alert('Please provide valid PIN number');
+        return;
+    }
+
+
+
+    const newCashedOutBalance = availableBalance - cashOutAmount;
+
+    document.getElementById('available-balance').innerText = newCashedOutBalance;
+
+
+})
+
+
+
 // toggle system
 
 document.getElementById('btn-cash-out').addEventListener('click',function(){
+    // button color while clicked
+     document.getElementById('btn-cash-out').classList.add('button-click-style');
+
+     document.getElementById('btn-adding-money').classList.remove('button-click-style');
+
+    //  button toggle
+
     document.getElementById('add-money-section').style.display = 'none';
 
     document.getElementById('cash-out-section').style.display = 'block';
 })
 document.getElementById('btn-adding-money').addEventListener('click',function(){
+    // button color while clicked
+    document.getElementById('btn-adding-money').classList.add('button-click-style');
+
+     document.getElementById('btn-cash-out').classList.remove('button-click-style');
+
+    // button toggle
+
     document.getElementById('cash-out-section').style.display = 'none';
 
     document.getElementById('add-money-section').style.display = 'block';
 })
 
 
-
-// cash out/ cash withdraw feature
-
-document.getElementById('btn-withdraw-money').addEventListener('click',function(event){
-    event.preventDefault();
-})
