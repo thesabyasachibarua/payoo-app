153ff020cd3c3545e36b1e889115aa6330df1437
Pedp4 WPBX4125BLF092403369 <wpbx4125blf092403369@pedp4.onmicrosoft.com>
2025-08-28 15:30:12 +0600

    transaction part added

--- home.js
+++ home.js
@@ -3,6 +3,8 @@ console.log('home page connected');
 const validAccountNumber = 01991949385;
 const validPinNumber = 9876;
 
+const alltransaction = [];
+
 // reusable functions added:
 
 // functions for accessing value from input converted to INT =>
@@ -16,6 +18,31 @@ const validPinNumber = 9876;
         return intValueInputField;
 
     }
+
+    // toggle handle function
+
+    function handleToggle(id){
+        const forms = document.getElementsByClassName('form');
+    for(const form of forms){
+        form.style.display = 'none';
+    }
+
+    document.getElementById(id).style.display = 'block';
+    }
+
+    // toggle style handle function
+
+    function toggleStyle(id){
+
+        const buttonStyle = document.getElementsByClassName('btn-style');
+
+        for(const btnStyle of buttonStyle){
+            btnStyle.classList.remove('button-click-style');
+        }
+
+        document.getElementById(id).classList.add('button-click-style');
+    }
+    
 
 
 
@@ -36,15 +63,22 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
         alert('Please provide valid Account Number');
         return;
     }
-
     if(pinNumber !== validPinNumber){
         alert('Please provide valid PIN number');
         return;
     }
-
     const newAddedBalance = addAmount + availableBalance;
 
     document.getElementById('available-balance').innerText = newAddedBalance;
+
+    const addedTransaction = {
+        name: 'Add Money',
+        date: new Date().toLocaleTimeString()
+    }
+
+    alltransaction.push(addedTransaction);
+    // console.log(alltransaction);
+    
 })
 
 
@@ -72,15 +106,21 @@ document.getElementById('btn-withdraw-money').addEventListener('click',function(
         alert('Please provide valid PIN number');
         return;
     }
-
-
-
     const newCashedOutBalance = availableBalance - cashOutAmount;
 
     document.getElementById('available-balance').innerText = newCashedOutBalance;
 
+    const addedTransaction = {
+        name: 'Cash Out',
+        date: new Date().toLocaleTimeString()
+    }
 
+    alltransaction.push(addedTransaction);
 })
+
+// transaction feature
+
+
 
 
 
@@ -88,27 +128,69 @@ document.getElementById('btn-withdraw-money').addEventListener('click',function(
 
 document.getElementById('btn-cash-out').addEventListener('click',function(){
     // button color while clicked
-     document.getElementById('btn-cash-out').classList.add('button-click-style');
+    //  document.getElementById('btn-cash-out').classList.add('button-click-style');
+    //  document.getElementById('btn-adding-money').classList.remove('button-click-style');
 
-     document.getElementById('btn-adding-money').classList.remove('button-click-style');
+     toggleStyle('btn-cash-out');
 
     //  button toggle
 
-    document.getElementById('add-money-section').style.display = 'none';
+    handleToggle('cash-out-section');
 
-    document.getElementById('cash-out-section').style.display = 'block';
+
+
 })
 document.getElementById('btn-adding-money').addEventListener('click',function(){
     // button color while clicked
-    document.getElementById('btn-adding-money').classList.add('button-click-style');
 
-     document.getElementById('btn-cash-out').classList.remove('button-click-style');
+    toggleStyle('btn-adding-money')
 
     // button toggle
 
-    document.getElementById('cash-out-section').style.display = 'none';
+    handleToggle('add-money-section')
+})
 
-    document.getElementById('add-money-section').style.display = 'block';
+document.getElementById('btn-transfer-money').addEventListener('click',function(){
+    toggleStyle('btn-transfer-money');
+
+    handleToggle('transfer-money');
+})
+
+document.getElementById('btn-transaction').addEventListener('click',function(){
+    toggleStyle('btn-transaction');
+    handleToggle('transaction-money');
+
+    document.getElementById('transaction-money').innerText = '';
+
+    for(const data of alltransaction){
+
+        const newElement = document.createElement('div');
+
+
+
+        newElement.innerHTML = `
+            <div class="flex justify-between items-center bg-white rounded-3xl ">
+                <div class="flex justify-around items-center">
+                    <img class=" m-3 rounded-full p-4 bg-[#F4F5F7]" src="./assets/wallet1.png" alt="">
+                    <div>
+                        <h1 class="text-[18px] font-bold">${data.name}</h1>
+                        <p font-[400] text-[#08080880] text-[14px]>${data.date}</p>
+                    </div>
+                </div>
+                <div>
+                    <i class="fa-solid fa-ellipsis-vertical mr-4"></i>
+                </div>
+            </div>
+            
+
+        </div>
+    `
+
+    document.getElementById('transaction-money').appendChild(newElement); 
+
+    }
+
+
 })
 
 
